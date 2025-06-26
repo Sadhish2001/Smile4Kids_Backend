b3d30b0e0280a8d496eb54599053389a95e3a8fd
Geethuaahasolutions <geethuaahasolutions03@gmail.com>
2025-06-26 15:31:51 +0530

    app.js

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME || 'mobile_backend',
+  database: process.env.DB_NAME,
   port: process.env.DB_PORT
 });
 
